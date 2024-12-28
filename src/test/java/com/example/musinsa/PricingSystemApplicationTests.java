58fd8f9eba245ab734251cd065952e393a96ee94
ShinDohee <intei0909@gmail.com>
2024-12-28 22:34:03 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/musinsa/PricingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musinsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
